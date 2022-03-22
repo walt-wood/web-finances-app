806ee3a8180caec8cc6cd4381ac8b0c0863e8a03
wwood44 <80364037+wwood44@users.noreply.github.com>
2022-03-22 13:52:37 -0500

    Added ExpenseDate component and css

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  // toLocaleString() is JS helper method to extract date info in specific formats.
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__year">{year}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
