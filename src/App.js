6e86ee84b49d1a8e4ef74c6c8b7f9eaa46a3fa79
wwood44 <80364037+wwood44@users.noreply.github.com>
2022-03-22 19:19:03 -0500

    Through #55

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -25,7 +26,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  // useState() cannot be called outside component functions or inside nested functions
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    // This schedules the update, so a console.log(title) here would show old value.
+    setTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
