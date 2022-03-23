59ee6b0438eed1fb76571169b7eddbaa96e706c2
wwood44 <80364037+wwood44@users.noreply.github.com>
2022-03-23 09:13:29 -0500

    Through #56

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,53 +4,61 @@ import { useState } from "react";
 const ExpenseForm = () => {
   // Multiple state slice approach
   // target.value is always a string so send useState empty string.
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
-  //   const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
 
   // Above approach is not necessary by passing object to useState({})
-  const [userInput, setUserInput] = useState({
-    enterdTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
   const titleChangeHandler = (event) => {
     // Multiple state approach
-    // setEnteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
 
     // Single state approach
     // Since React replaces old state with this update, must give it all data.
-    setUserInput({
-      // Copy old object
-      ...userInput,
-      // Update title
-      enterdTitle: event.target.value,
-    });
+    // setUserInput({
+    //   // Copy old object
+    //   ...userInput,
+    //   // Update title
+    //   enteredTitle: event.target.value,
+    // });
+
+    // This approach guaranties the previous state is the one we want. The latest previous state.
+    // setUserInput((prevState) => {
+    //   return {
+    //     ...prevState,
+    //     entertedTitle: event.target.value,
+    //   };
+    // });
   };
 
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      // Copy old object
-      ...userInput,
-      // Update title
-      enterdAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   // Copy old object
+    //   ...userInput,
+    //   // Update title
+    //   enterdAmount: event.target.value,
+    // });
   };
 
   const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      // Copy old object
-      ...userInput,
-      // Update title
-      enteredDate: event.target.value,
-    });
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   // Copy old object
+    //   ...userInput,
+    //   // Update title
+    //   enteredDate: event.target.value,
+    // });
   };
 
   return (
-    <form>
+    <form onSubmit={}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
